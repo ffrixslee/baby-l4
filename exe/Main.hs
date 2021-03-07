e8ac1af4228b0dde2b0a9bdbd3e22590a594010b
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-03-07 16:26:01 +0800

    Replicated previous commandline behavior with optparse-applicative

--- exe/Main.hs
+++ exe/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Parser (parseProgram)
 import Typing (tpProgram)
 import System.Environment
+import Options.Applicative
 import qualified ToGF as GF
 
 process :: FilePath -> String -> IO ()
@@ -17,11 +18,35 @@ process filepath input = do
       putStrLn "Parser Error:"
       print err
 
+data Format  = Fall | Fgf GFlang deriving Show
+data GFlang  = GFeng | GFmalay deriving Show
+
+data InputOpts = InputOpts
+  { format   :: Format
+  , filepath :: FilePath --Maybe FilePath
+  } deriving Show
+
+optsParse :: Parser InputOpts
+optsParse = InputOpts <$>
+              subparser
+                ( command "all" (info (pure Fall) (progDesc "Prints all available formats"))
+               <> command "gf" (info gfSubparser gfHelper))
+            <*> argument str (metavar "Filename")
+        where
+          gfSubparser = subparser ( command "en" (info (pure (Fgf GFeng))   (progDesc "tell GF to output english"))
+                                 <> command "my" (info (pure (Fgf GFmalay)) (progDesc "tell GF to output malay"))
+                                  )
+                        <**> helper
+          gfHelper = fullDesc
+                  <> header "l4 gf - specialized for natLang output"
+                  <> progDesc "Prints natLang format (subcommands: en, my)"
+
+
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    []      -> putStrLn "Usage: core-language <input file>"
-    [fname] -> do
-      contents <- readFile fname
-      process fname contents
+  let optsParse' = info (optsParse <**> helper) ( fullDesc
+                                               <> header "mini-l4 - minimum l4? miniturised l4?")
+  opts <- customExecParser (prefs showHelpOnError) optsParse'
+
+  contents <- readFile $ filepath opts
+  process (filepath opts) contents
